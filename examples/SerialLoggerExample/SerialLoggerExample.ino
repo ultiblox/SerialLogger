ef80d7c674e99a11a17157d1aba2fe00398fa64c
Maker Zone <dev@makerzone.com.au>
2024-09-23 10:05:33 +1000

    Overhaul

--- examples/SerialLoggerExample/SerialLoggerExample.ino
+++ examples/SerialLoggerExample/SerialLoggerExample.ino
@@ -1,24 +1,24 @@
-// SimpleLogger Example
-#include "LoggerConfig.h"
 #include "SerialLogger.h"
+
+// Define the Logger instance here
+SerialLogger Logger;
 
 int loopNumber = 1;
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("Starting system...");
-    LOG_INFO("Starting system...");
-    LOG_WARN("This is a warning message.");
-    LOG_ERROR("This is an error message.");
-    LOG_DEBUG("This is a debug message.");
+    Logger.init(115200);  // Initialize Serial communication
+    Logger.logln("Starting system...");
+
+    Logger.logln("This is an info message.");
+    Logger.logln("This is a warning message.");
+    Logger.logln("This is an error message.");
+    Logger.logln("This is a debug message.");
 }
 
 void loop() {
-    LOG_INFO("Loop running...");
+    Logger.log("Loop running... ");
+    Logger.logln(loopNumber);
 
-    LOG_DEBUG("Loop number: " + String(loopNumber));
     delay(1000);  // Simulate some work
     loopNumber++;
 }
-
-
--- src/SerialLogger.h
+++ src/SerialLogger.h
@@ -1,49 +1,78 @@
-// SerialLogger.h
-#ifndef SERIAL_LOGGER_H
-#define SERIAL_LOGGER_H
+#ifndef _SERIALLOGGER_H
+#define _SERIALLOGGER_H
 
 #include <Arduino.h>
 
-#if SERIAL_LOGGER_ENABLED
+// Enable or disable logging at compile time
+#ifndef SERIAL_LOGGER_ENABLED
+    #define SERIAL_LOGGER_ENABLED 1  // Set to 0 to disable all logging
+#endif
 
-// Updated macros without F(msg)
-#define LOG_INFO(msg)  \
-  Serial.print(F("[INF] ")); \
-  Serial.print(F("Line ")); \
-  Serial.print(__LINE__); \
-  Serial.print(F(" - ")); \
-  Serial.println(msg)
+class SerialLogger {
+public:
+    // Initialize Serial if needed
+    void init(long baudRate = 115200) {
+        Serial.begin(baudRate);
+        while (!Serial) { ; }  // Wait for Serial to be ready
+    }
 
-#define LOG_WARN(msg)  \
-  Serial.print(F("[WRN] ")); \
-  Serial.print(F("Line ")); \
-  Serial.print(__LINE__); \
-  Serial.print(F(" - ")); \
-  Serial.println(msg)
+    // Logging functions (enabled/disabled based on SERIAL_LOGGER_ENABLED)
+    template<typename T>
+    void log(T content) {
+        if (SERIAL_LOGGER_ENABLED) {
+            Serial.print(content);
+        }
+    }
 
-#define LOG_ERROR(msg) \
-  Serial.print(F("[ERR] ")); \
-  Serial.print(F("Line ")); \
-  Serial.print(__LINE__); \
-  Serial.print(F(" - ")); \
-  Serial.println(msg)
+    template<typename T>
+    void logln(T content) {
+        if (SERIAL_LOGGER_ENABLED) {
+            Serial.println(content);
+        }
+    }
 
-#define LOG_DEBUG(msg) \
-  Serial.print(F("[DBG] ")); \
-  Serial.print(F("Line ")); \
-  Serial.print(__LINE__); \
-  Serial.print(F(" - ")); \
-  Serial.println(msg)
+    // Print functions (always work like Serial.print/println, regardless of logging flag)
+    template<typename T>
+    void print(T content) {
+        Serial.print(content);
+    }
 
-#else
+    template<typename T>
+    void println(T content) {
+        Serial.println(content);
+    }
+    
+    // Overloaded versions for multiple data types
+    template<typename T1, typename T2>
+    void log(T1 content1, T2 content2) {
+        if (SERIAL_LOGGER_ENABLED) {
+            Serial.print(content1);
+            Serial.print(content2);
+        }
+    }
 
-// Empty macros when logging is disabled
-#define LOG_INFO(msg)
-#define LOG_WARN(msg)
-#define LOG_ERROR(msg)
-#define LOG_DEBUG(msg)
+    template<typename T1, typename T2>
+    void logln(T1 content1, T2 content2) {
+        if (SERIAL_LOGGER_ENABLED) {
+            Serial.print(content1);
+            Serial.println(content2);
+        }
+    }
 
-#endif // SERIAL_LOGGER_ENABLED
+    template<typename T1, typename T2>
+    void print(T1 content1, T2 content2) {
+        Serial.print(content1);
+        Serial.print(content2);
+    }
 
-#endif // SERIAL_LOGGER_H
+    template<typename T1, typename T2>
+    void println(T1 content1, T2 content2) {
+        Serial.print(content1);
+        Serial.println(content2);
+    }
+};
 
+// Declare Logger as an external variable
+extern SerialLogger Logger;
+
+#endif  // _SERIALLOGGER_H
