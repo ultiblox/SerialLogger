e0f512a099537fe083ba5305974f54a287911ffb
Maker Zone <dev@makerzone.com.au>
2024-11-06 15:22:54 +1100

    Fixing version script

--- version-increment.sh
+++ version-increment.sh
@@ -76,5 +76,6 @@ git commit -m "Update library.properties to version $new_version"
 # Create and push the new tag
 git tag "v${new_version}"
 git push origin "v${new_version}"
+git push origin main
 
 echo $new_version > VERSION
